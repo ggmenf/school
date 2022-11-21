38f342b35312f1b44b590bfe88c0e8b72c1c5753
PCashnK <ao5230229@gmail.com>
2022-11-21 14:51:51 +0300

    перенеcено расписание (timetable) в отдельный модуль: timetable.py

---
+++ timetable.py
@@ -0,0 +1,50 @@
+from data import school_objects, days, lesson_time
+
+timetable = [
+
+    [
+        school_objects[9],
+        school_objects[10],
+        school_objects[0],
+        school_objects[7],
+        school_objects[8],
+    ],
+    [
+        school_objects[1],
+        school_objects[4],
+        school_objects[10],
+        school_objects[3],
+        school_objects[11],
+        school_objects[0],
+    ],
+    [
+        school_objects[2],
+        school_objects[2],
+        school_objects[9],
+        school_objects[5],
+        school_objects[0],
+        school_objects[3],
+    ],
+    [
+        school_objects[4],
+        school_objects[11],
+        school_objects[6],
+        school_objects[0],
+        school_objects[12],
+        school_objects[5],
+    ],
+    [
+        school_objects[9],
+        school_objects[4],
+        school_objects[5],
+        school_objects[11],
+        school_objects[6],
+        school_objects[0],
+    ],
+    [
+        ['выходной'],
+    ],
+    [
+        ['выходной'],
+    ],
+]
