57d6ab5921ae8b7d72e29de03afa0b2a79c4cda7
PCashnK <ao5230229@gmail.com>
2022-11-21 15:35:04 +0300

    удалено время для выходных дней

--- main.py
+++ main.py
@@ -4,5 +4,8 @@ from timetable import timetable
 for i in range(len(timetable)):
     print(f'{days[i]}:')
     for j in range(len(timetable[i])):
-        print(f'\t{lesson_time[j]}:  {timetable[i][j]}')
+        if timetable[i][j] == ['выходной']:
+            print(f'\t{timetable[i][j]}')
+        else:
+            print(f'\t{lesson_time[j]}:  {timetable[i][j]}')
     print()
