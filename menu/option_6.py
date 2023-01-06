f756d6c562d6843fdfd5d1898c86b53ca2d63d86
Anton Akhramovich <akhramovich.anton@gmail.com>
2023-01-06 16:33:22 +0300

    released function remove_timetable()

--- menu/option_6.py
+++ menu/option_6.py
@@ -1,2 +1,10 @@
-def option_6():
-    print('option-6')
+import os
+
+from utils import print_list_of_saved_timetables
+
+
+def remove_timetable():
+    print_list_of_saved_timetables()
+    timetable_name = input('Введите имя расписания: ')
+    os.remove(f'{timetable_name}.txt')
+    print(f'Расписание {timetable_name} удалено.')
