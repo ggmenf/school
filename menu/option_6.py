5d76cb040b2fd28935187915e271f2e15fc7112f
Anton Akhramovich <akhramovich.anton@gmail.com>
2023-01-06 22:19:15 +0300

    added if for print_timetable_names()

--- menu/option_6.py
+++ menu/option_6.py
@@ -1,10 +1,14 @@
 import os
 
-from utils import print_list_of_timetable_names
+from utils import print_timetable_names
 
 
 def remove_timetable():
-    print_list_of_timetable_names()
-    timetable_name = input('Введите имя расписания: ')
-    os.remove(f'{timetable_name}.bin')
-    print(f'Расписание {timetable_name} удалено.')
+    if print_timetable_names():
+        timetable_name = input('Введите имя расписания: ')
+        file_name = f'{timetable_name}.bin'
+        if os.path.exists(file_name):
+            os.remove(file_name)
+            print(f'Расписание {timetable_name} удалено.')
+        else:
+            print(f'Ошибка: файл {file_name} не найден')
