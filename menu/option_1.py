ffee68493a93643b6168c209dd47825ed795e214
Anton Akhramovich <akhramovich.anton@gmail.com>
2023-01-06 22:18:27 +0300

    added if for print_timetable_names()

--- menu/option_1.py
+++ menu/option_1.py
@@ -1,18 +1,16 @@
 from utils import (
-    get_timetable_from_file,
-    print_list_of_timetable_names
+    get_timetable,
+    print_timetable_names
 )
 
 
 def print_timetable():
-    print_list_of_timetable_names()
-
-    timetable_name = input('Введите имя расписания: ')
-
-    for day, items in get_timetable_from_file(timetable_name).items():
-        print()
-        print(f'{day}:')
-        i = 1
-        for item in items:
-            print(f'  {i}. {item}')
-            i += 1
+    if print_timetable_names():
+        timetable_name = input('Введите имя расписания: ')
+        for day, items in get_timetable(timetable_name).items():
+            print()
+            print(f'{day}:')
+            i = 1
+            for item in items:
+                print(f'  {i}. {item}')
+                i += 1
