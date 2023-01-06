7638abdb15022a9ab9423c92a71d9b5996598987
Anton Akhramovich <akhramovich.anton@gmail.com>
2023-01-06 19:39:24 +0300

    renamed functions and added new function

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,25 +3,30 @@ import pickle
 from glob import glob
 
 
-def get_list_of_saved_timetables():
+def get_list_of_timetable_names():
     list_of_saved_timetables = []
     for timetable_name in glob("*.txt"):
         list_of_saved_timetables.append(timetable_name)
     return list_of_saved_timetables
 
 
-def print_list_of_saved_timetables():
+def print_list_of_timetable_names():
     print('Список расписаний:')
-    for timetable_name in get_list_of_saved_timetables():
+    for timetable_name in get_list_of_timetable_names():
         print(f' - {timetable_name.split(".")[0]}')
     print()
 
 
-def get_timetable_from_file(timetable_name):
+def get_timetable_from_file(file_name):
     timetable = {}
     try:
-        with open(f"{timetable_name}.txt", 'rb') as f:
+        with open(f"{file_name}.bin", 'rb') as f:
             timetable = pickle.load(f)
     except FileNotFoundError as e:
-        print(f'ERROR: расписания \"{timetable_name}\" нет в списке.')
+        print(f'ERROR: расписания \"{file_name}\" нет в списке.')
     return timetable
+
+
+def write_timetable_to_file(timetable, file_name):
+    with open(f'{file_name}.bin', 'wb') as f:
+        pickle.dump(timetable, f)
